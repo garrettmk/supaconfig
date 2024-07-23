81c2702d81edc2df01eec95511bd8d3aaec49da3
Garrett Myrick <garrettmyrick@gmail.com>
2024-07-23 14:38:19 -0700

    added makeUrl utility

--- lib/utils/url.ts
+++ lib/utils/url.ts
@@ -5,3 +5,33 @@ export function getFromSearchParams(searchParams: URLSearchParams | ReadonlyURLS
     ? searchParams.get(key) ?? defaultValue
     : searchParams[key] ?? defaultValue;
 }
+
+
+export type MakeUrlInput = {
+  baseUrl?: string;
+  searchParams?: Record<string, string>;
+  set?: Record<string, string>;
+  delete?: string[];
+}
+
+export function makeUrl({
+  baseUrl = '',
+  searchParams = {},
+  set = {},
+  delete: deleteValues = []
+}: MakeUrlInput) {
+  const newSearchParams = new URLSearchParams(searchParams);
+
+  for (const [key, value] of Object.entries(set)) {
+    newSearchParams.set(key, value);
+  }
+
+  for (const key of deleteValues) {
+    newSearchParams.delete(key);
+  }
+
+  if (newSearchParams.size)
+    return `${baseUrl}?${newSearchParams.toString()}`;
+  else
+    return baseUrl;
+}
