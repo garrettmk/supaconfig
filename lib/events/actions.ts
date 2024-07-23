06611af549e61145949eca56676d2298213dd654
Garrett Myrick <garrettmyrick@gmail.com>
2024-07-23 14:37:30 -0700

    added sorting to getEventStream action

--- lib/events/actions.ts
+++ lib/events/actions.ts
@@ -4,26 +4,33 @@ import { type PaginationInput, type PaginationResult } from "@/lib/pagination";
 import { type Event } from "@/lib/events/types";
 import { createClient } from "@/lib/supabase/server";
 import { PostgrestError } from "@supabase/supabase-js";
+import { SortingInput, SortingResult } from "../sorting";
 
 
-export type GetEventStreamInput = PaginationInput & {
+export type GetEventStreamInput = PaginationInput & SortingInput & {
   aggregateId: string;
 }
 
-export type GetEventStreamResult = PaginationResult & {
+export type GetEventStreamResult = PaginationResult & SortingResult & {
   data?: Event[];
   error?: PostgrestError; 
 };
 
 export async function getEventStream(input: GetEventStreamInput): Promise<GetEventStreamResult> {
-  const { aggregateId, offset = 0, limit = 10 } = input;
+  const { 
+    aggregateId, 
+    offset = 0, 
+    limit = 10, 
+    sortKey = 'version_number',
+    sortDirection = 'asc'
+  } = input;
 
   const supabase = createClient();
   const { data, error, count } = await supabase
     .from('events')
     .select('*, created_by (id, name)', { count: 'exact'})
     .eq('aggregate_id', aggregateId)
-    .order('version_number', { ascending: true })
+    .order(sortKey, { ascending: sortDirection === 'asc' })
     .range(offset, offset + limit - 1);
 
   if (error)
@@ -34,6 +41,8 @@ export async function getEventStream(input: GetEventStreamInput): Promise<GetEve
     error: error ?? undefined,
     count: count ?? 0,
     offset,
-    limit
+    limit,
+    sortKey,
+    sortDirection
   };
 }
