ba0de222cb8ecbc1430bb99e9447bf6cd9e91bf3
Garrett Myrick <garrettmyrick@gmail.com>
2024-07-30 19:59:57 -0700

    added options param to useUrlState

--- lib/utils/use-url-state.ts
+++ lib/utils/use-url-state.ts
@@ -1,5 +1,6 @@
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { makeUrl } from "./url";
+import { type NavigateOptions } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 
 export function useUrlState(key: string) {
@@ -8,12 +9,15 @@ export function useUrlState(key: string) {
   const router = useRouter();
   
   const value = searchParams.get(key);
-  const setValue = (newValue: string | null) => router.push(makeUrl({
-    baseUrl: pathname,
-    searchParams,
-    set: newValue ? { [key]: newValue } : {},
-    delete: newValue ? [] : [key]
-  }));
+  const setValue = (newValue: string | null, options?: NavigateOptions) => router.push(
+    makeUrl({
+      baseUrl: pathname,
+      searchParams,
+      set: newValue ? { [key]: newValue } : {},
+      delete: newValue ? [] : [key]
+    }),
+    options
+  );
 
   return [value, setValue] as const;
 }
